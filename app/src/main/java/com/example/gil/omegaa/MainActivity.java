dbfcb4273f11aaa3ff7508a39b55973a183649b9
Younggil Lee <leyoungg@sheridancollege.ca>
2017-06-06 13:01:06 -0400

    Create chat page and button click enent to move chat page from manipage

--- app/src/main/java/com/example/gil/omegaa/MainActivity.java
+++ app/src/main/java/com/example/gil/omegaa/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.gil.omegaa;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin;
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this, ChatActivity.class);
+                startActivity(in);
+            }
+        });
     }
+
+
+/*    public void onClick(View v){
+        Intent intent=null;
+        switch(v.getId()){
+            case R.id.btnLogin:
+                intent = new Intent(this, ChatActivity.class);
+                break;
+        }
+        startActivity(intent);
+    }*/
 }
