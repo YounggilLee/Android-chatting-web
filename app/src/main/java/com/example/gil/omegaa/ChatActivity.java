475964d0851c66cdc9afc77d5d1761a8b0e782d7
Younggil Lee <leyoungg@sheridancollege.ca>
2017-06-07 12:22:11 -0400

    Implement recyclerview and create MyAdapter class to handle recyclerviewer for displaying data

--- app/src/main/java/com/example/gil/omegaa/ChatActivity.java
+++ app/src/main/java/com/example/gil/omegaa/ChatActivity.java
@@ -2,10 +2,16 @@ package com.example.gil.omegaa;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
 public class ChatActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    String[] myDataset = { "Hi", "Do", "you", "want","to","watch", "movie"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +27,21 @@ public class ChatActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+
     }
 }
